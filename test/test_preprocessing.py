3f413e08a39c92a8c42679ae2bf84b43275a554e
Niklas Kemper <65356992+Niklas-23@users.noreply.github.com>
2022-10-27 18:46:27 +0200

    remove test

--- test/test_preprocessing.py
+++ test/test_preprocessing.py
@@ -1,5 +1,4 @@
-from preprocessing.preprocessing import get_preprocessed_brfss_train_test_split, \
-    get_preprocessed_brfss_train_test_split_one_hot_encoded
+from preprocessing.preprocessing import get_preprocessed_brfss_train_test_split
 
 
 class TestPreprocessing:
@@ -10,8 +9,3 @@ class TestPreprocessing:
         assert test_data is not None, "test_data not None"
         assert train_target is not None, "train_target not None"
         assert test_target is not None, "test_target not None"
-
-    def test_one_hot_encoding(self):
-        train_data, test_data, train_target, test_target = get_preprocessed_brfss_train_test_split_one_hot_encoded()
-        assert train_target.size >= 2, "Should be >=2"
-        assert test_target.size >= 2, "Should be >=2"
