6c82804cf5c06e8d10bd6dc62c6b9a2509d03b55
Niklas Kemper <info@niklas-kemper.de>
2022-11-07 16:18:33 +0100

    add test for one hot encoded preprocessing

--- test/test_preprocessing.py
+++ test/test_preprocessing.py
@@ -1,4 +1,5 @@
 from preprocessing.preprocessing import get_train_test_split, get_preprocessed_brfss_dataset
+from preprocessing.preproccessing_one_hot_encoding import get_preprocessed_brfss_dataset_one_hot_encoded
 
 
 class TestPreprocessing:
@@ -10,3 +11,8 @@ class TestPreprocessing:
         assert test_data is not None, "test_data not None"
         assert train_target is not None, "train_target not None"
         assert test_target is not None, "test_target not None"
+
+    def test_get_preprocessed_brfss_dataset_one_hot_encoded(self):
+        dataset, target = get_preprocessed_brfss_dataset_one_hot_encoded()
+        assert dataset is not None, "dataset not None"
+        assert target is not None, "target not None"
