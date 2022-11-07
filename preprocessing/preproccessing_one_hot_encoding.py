ac0be1734b57153362aff94ecf46d465307f488b
johannzapf <johann.zapf@gmail.com>
2022-11-07 16:00:49 +0100

    reformat code a bit

--- preprocessing/preproccessing_one_hot_encoding.py
+++ preprocessing/preproccessing_one_hot_encoding.py
@@ -8,6 +8,15 @@ from preprocessing import get_preprocessed_brfss_dataset, diabetes_columns, over
     undersample_dataset
 
 
+# Excludes yes/no columns
+ordinal_columns = ["GenHealth", "Checkup", "HighBP", "Income", "Age", "BMI", "Education", "Smoking", "PhysActivity"]
+
+# Includes yes/no columns
+all_ordinal_columns = ["GenHealth", "Healthcare", "MedCost", "Checkup", "HighBP", "HighChol", "HeartAttack", "AngiCoro",
+                       "Stroke", "Asthma", "Arthritis", "Kidney", "Sex", "Income", "SodiumSalt", "Age", "BMI",
+                       "Education", "Alcohol", "Smoking", "FruitCons", "VegetCons", "PhysActivity", "Muscles"]
+
+
 # Returns preprocessed dataset where all columns with ordinal values that are not simply yes/no are one hot encoded
 # If the parameter is set to true, the target column will also be one hot encoded
 # This function does not include any sampling
@@ -15,12 +24,10 @@ def get_preprocessed_brfss_dataset_one_hot_encoded(target_one_hot_encoded=False)
     dataset, target = get_preprocessed_brfss_dataset()
     one_hot_encoder = OneHotEncoder()
     encoded = pd.DataFrame(
-        one_hot_encoder.fit_transform(dataset[["GenHealth", "Checkup", "HighBP", "Income", "Age", "BMI",
-                                               "Education", "Smoking", "PhysActivity"]]).toarray(),
+        one_hot_encoder.fit_transform(dataset[ordinal_columns]).toarray(),
         columns=one_hot_encoder.get_feature_names_out())
     dataset = dataset.join(encoded)
-    dataset = dataset.drop(columns=["GenHealth", "Checkup", "HighBP", "Income", "Age", "BMI", "Education", "Smoking",
-                                    "PhysActivity"])
+    dataset = dataset.drop(columns=ordinal_columns)
     if target_one_hot_encoded:
         target = pd.DataFrame(one_hot_encoder.fit_transform(target).toarray(), columns=diabetes_columns)
     return dataset, target
@@ -34,19 +41,10 @@ def get_preprocessed_brfss_dataset_one_hot_encoded_all_columns(target_one_hot_en
     dataset, target = get_preprocessed_brfss_dataset()
     one_hot_encoder = OneHotEncoder()
     encoded = pd.DataFrame(
-        one_hot_encoder.fit_transform(dataset[["GenHealth", "Healthcare", "MedCost", "Checkup",
-                                               "HighBP", "HighChol", "HeartAttack", "AngiCoro", "Stroke", "Asthma",
-                                               "Arthritis", "Kidney",
-                                               "Sex", "Income", "SodiumSalt", "Age", "BMI", "Education", "Alcohol",
-                                               "Smoking", "FruitCons", "VegetCons", "PhysActivity",
-                                               "Muscles"]]).toarray(),
+        one_hot_encoder.fit_transform(dataset[all_ordinal_columns]).toarray(),
         columns=one_hot_encoder.get_feature_names_out())
     dataset = dataset.join(encoded)
-    dataset = dataset.drop(columns=["GenHealth", "Healthcare", "MedCost", "Checkup",
-                                    "HighBP", "HighChol", "HeartAttack", "AngiCoro", "Stroke", "Asthma", "Arthritis",
-                                    "Kidney", "Sex", "Income", "SodiumSalt", "Age", "BMI", "Education", "Alcohol",
-                                    "Smoking", "FruitCons", "VegetCons", "PhysActivity",
-                                    "Muscles"])
+    dataset = dataset.drop(columns=all_ordinal_columns)
     if target_one_hot_encoded:
         target = pd.DataFrame(one_hot_encoder.fit_transform(target).toarray(), columns=diabetes_columns)
     return dataset, target
