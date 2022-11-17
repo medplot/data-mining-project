7a28e55cb9bd65720ad969e498bc7aa4ad3d87c6
SimonEndres <66074656+SimonEndres@users.noreply.github.com>
2022-11-17 15:42:10 +0100

    Merge remote-tracking branch 'origin/train_validation_split' into k_nearest_neighbor-centroid

--- preprocessing/preprocessing_label_encoding.py
+++ preprocessing/preprocessing_label_encoding.py
@@ -1,9 +1,9 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 from pandas import DataFrame
 
 from preprocessing.preprocessing import get_preprocessed_brfss_dataset, oversample_dataset, undersample_dataset, \
-    get_train_test_split
+    get_train_validation_test_split
 
 
 # Returns preprocessed dataset where all ordinal values keep their label encoding.
@@ -12,27 +12,47 @@ def get_preprocessed_brfss_dataset_label_encoded() -> Tuple[DataFrame, DataFrame
     return get_preprocessed_brfss_dataset()
 
 
-# Returns a train/test split of the preprocessed dataset where all ordinal values keep their label encoding.
+# Returns a train/validation split of the preprocessed dataset where all ordinal values keep their label encoding.
+# If the parameter include_test_data is set to true, test data will also be returned.
 # This function does not include any sampling.
-def get_preprocessed_brfss_dataset_label_encoded_train_test_split() \
-        -> Tuple[DataFrame, DataFrame, DataFrame, DataFrame]:
+def get_preprocessed_brfss_dataset_label_encoded_train_test_split(include_test_data=False) \
+        -> Union[Tuple[DataFrame, DataFrame, DataFrame, DataFrame], Tuple[
+            DataFrame, DataFrame, DataFrame, DataFrame, DataFrame, DataFrame]]:
     dataset, target = get_preprocessed_brfss_dataset_label_encoded()
-    return get_train_test_split(dataset, target)
+    return get_train_validation_test_split(dataset, target, include_test_data)
 
 
-# Returns a train/test split of the preprocessed dataset where all ordinal values keep their label encoding.
+# Returns a train/validation split of the preprocessed dataset where all ordinal values keep their label encoding.
+# If the parameter include_test_data is set to true, test data will also be returned.
 # This function includes oversampling
-def get_preprocessed_brfss_dataset_label_encoded_train_test_split_oversampled() \
-        -> Tuple[DataFrame, DataFrame, DataFrame, DataFrame]:
-    data_train, data_test, target_train, target_test = get_preprocessed_brfss_dataset_label_encoded_train_test_split()
-    data_train, target_train = oversample_dataset(data_train, target_train)
-    return data_train, data_test, target_train, target_test
+def get_preprocessed_brfss_dataset_label_encoded_train_test_split_oversampled(include_test_data=False) \
+        -> Union[Tuple[DataFrame, DataFrame, DataFrame, DataFrame], Tuple[
+            DataFrame, DataFrame, DataFrame, DataFrame, DataFrame, DataFrame]]:
+    if include_test_data:
+        data_train, data_validation, data_test, target_train, target_validation, target_test = \
+            get_preprocessed_brfss_dataset_label_encoded_train_test_split(include_test_data)
+        data_train, target_train = oversample_dataset(data_train, target_train)
+        return data_train, data_validation, data_test, target_train, target_validation, target_test
+    else:
+        data_train, data_validation, target_train, target_validation = \
+            get_preprocessed_brfss_dataset_label_encoded_train_test_split(include_test_data)
+        data_train, target_train = oversample_dataset(data_train, target_train)
+        return data_train, data_validation, target_train, target_validation
 
 
-# Returns a train/test split of the preprocessed dataset where all ordinal values keep their label encoding.
+# Returns a train/validation split of the preprocessed dataset where all ordinal values keep their label encoding.
+# If the parameter include_test_data is set to true, test data will also be returned.
 # This function includes oversampling
-def get_preprocessed_brfss_dataset_label_encoded_train_test_split_undersampled() \
-        -> Tuple[DataFrame, DataFrame, DataFrame, DataFrame]:
-    data_train, data_test, target_train, target_test = get_preprocessed_brfss_dataset_label_encoded_train_test_split()
-    data_train, target_train = undersample_dataset(data_train, target_train)
-    return data_train, data_test, target_train, target_test
+def get_preprocessed_brfss_dataset_label_encoded_train_test_split_undersampled(include_test_data=False) \
+        -> Union[Tuple[DataFrame, DataFrame, DataFrame, DataFrame], Tuple[
+            DataFrame, DataFrame, DataFrame, DataFrame, DataFrame, DataFrame]]:
+    if include_test_data:
+        data_train, data_validation, data_test, target_train, target_validation, target_test = \
+            get_preprocessed_brfss_dataset_label_encoded_train_test_split(include_test_data)
+        data_train, target_train = undersample_dataset(data_train, target_train)
+        return data_train, data_validation, data_test, target_train, target_validation, target_test
+    else:
+        data_train, data_validation, target_train, target_validation = \
+            get_preprocessed_brfss_dataset_label_encoded_train_test_split(include_test_data)
+        data_train, target_train = undersample_dataset(data_train, target_train)
+        return data_train, data_validation, target_train, target_validation
